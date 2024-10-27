830fc8d3ff35e5877946355c21bda9b043da543f
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-10-28 01:02:28 +0800

    Update turnip_builder.sh

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -6,8 +6,8 @@ nocolor='\033[0m'
 deps="meson ninja patchelf unzip curl pip flex bison zip"
 workdir="$(pwd)/turnip_workdir"
 magiskdir="$workdir/turnip_module"
-ndkver="android-ndk-r26c"
-sdkver="27"
+ndkver="android-ndk-r27c"
+sdkver="33"
 mesasrc="https://gitlab.freedesktop.org/mesa/mesa/-/archive/main/mesa-main.zip"
 clear
 
